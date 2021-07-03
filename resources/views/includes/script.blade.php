7b6eea9390a63640d92d2e3e0531d2371c47502a
tantanrahman <social.tantanrahman@gmail.com>
2021-07-03 15:24:47 +0700

    Added and Deleted Bug JavaScript

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,5 +1,5 @@
 <script src="{{ url('plugins/jquery/jquery.min.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script><i class="ri-psychotherapy-line"></i>
 <script src="{{ url('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 <script>
   $.widget.bridge('uibutton', $.ui.button)
@@ -21,85 +21,100 @@
 @include('sweetalert::alert')
 
 <script>
-  $('#dropship-periode-start').datepicker({
-      format: "yyyy-mm-dd",
-      autoclose: true,
-	  endDate: "0d",
-  }).on('changeDate', function (e) {
-	  var dt = new Date(e.target.value);
-	  $("#dropship-periode-end").datepicker("setStartDate", dt);
+	$('#dropship-periode-start').datepicker({
+		format: "yyyy-mm-dd",
+		autoclose: true,
+		endDate: "0d",
+	}).on('changeDate', function (e) {
+		var dt = new Date(e.target.value);
+		$("#dropship-periode-end").datepicker("setStartDate", dt);
 
-	  fetch_dropship();
-  });
+		fetch_dropship();
+	});
 
-  $('#dropship-periode-end').datepicker({
-      format: "yyyy-mm-dd",
-      autoclose: true,
-	  endDate: "0d",
-  }).on('changeDate', function (e) {
-	  fetch_dropship();
-  });
+	$('#dropship-periode-end').datepicker({
+		format: "yyyy-mm-dd",
+		autoclose: true,
+		endDate: "0d",
+	}).on('changeDate', function (e) {
+		fetch_dropship();
+	});
 
-  $('body').on('click', '.dropship-export', function (e) {
-	  e.preventDefault();
+	$('body').on('click', '.dropship-export', function (e) {
+		e.preventDefault();
 
-	  let export_url = e.target.href,
-		  query = {
-			  date_start: $('#dropship-periode-start').val(),
-			  date_end: $('#dropship-periode-end').val(),
-		  },
-		  url_params = '';
+		let export_url = e.target.href,
+			query = {
+				date_start: $('#dropship-periode-start').val(),
+				date_end: $('#dropship-periode-end').val(),
+			},
+			url_params = '';
 
-	  url_params = '?' + $.param(query);
+		url_params = '?' + $.param(query);
 
-	  window.location.href = export_url + url_params;
-  });
+		window.location.href = export_url + url_params;
+	});
 
-  $('body').on('change', '#customer-type', function (e) {
-	  e.preventDefault();
+	$('body').on('change', '#customer-type', function (e) {
+		e.preventDefault();
 
-	  if (e.target.value == 'shipper')
-      {
-		  $('#customer-city-section').show();
-		  $('#customer-country-section').hide();
-		  $('#customer-country').val(106); // hardcoded to Indonesia for now
-      }
-	  else if (e.target.value == 'consignee')
-      {
-		  $('#customer-city-section').hide();
-		  $('#customer-country-section').show();
-		  $('#customer-city').val(1); // hack to bypass foreign key constraint fails
-      }
-	  else
-      {
-		  $('#customer-city-section').hide();
-		  $('#customer-country-section').hide();
-      }
-  });
+		if (e.target.value == 'shipper')
+		{
+			$('#customer-city-section').show();
+			$('#customer-country-section').hide();
+			$('#customer-country').val(106); // hardcoded to Indonesia for now
+		}
+		else if (e.target.value == 'consignee')
+		{
+			$('#customer-city-section').hide();
+			$('#customer-country-section').show();
+			$('#customer-city').val(1); // hack to bypass foreign key constraint fails
+		}
+		else
+		{
+			$('#customer-city-section').hide();
+			$('#customer-country-section').hide();
+		}
+	});
 
-  $('body').on('change', '#customer-city', function (e) {
-	  e.preventDefault();
 
-	  let customer_id_path = "{{ route('getCustomerId')  }}";
-	  $.get(customer_id_path, {source: 'city', id: e.target.value}).done((rsp) => {
-		  if (rsp.status)
-		  {
-			  $('#customer-id').val(rsp.customer_id);
-		  }
-	  });
-  });
+	$('body').on('change', '#select2userrole', function (e) {
+		e.preventDefault();
 
-  $('body').on('change', '#customer-country', function (e) {
-	  e.preventDefault();
+		if (e.target.value == 3)
+		{
+			$('#pic-marketing-user').show();
+		}
+		else
+		{
+			$('#pic-marketing-user').hide();
+		}
+	});
 
-	  let customer_id_path = "{{ route('getCustomerId')  }}";
-	  $.get(customer_id_path, {source: 'country', id: e.target.value}).done((rsp) => {
-		  if (rsp.status)
-		  {
-			  $('#customer-id').val(rsp.customer_id);
-		  }
-	  });
-  });
+	$('body').on('change', '#customer-city', function (e) {
+		e.preventDefault();
+
+		let customer_id_path = "{{ route('getCustomerId')  }}";
+		$.get(customer_id_path, {source: 'city', id: e.target.value}).done((rsp) => {
+			if (rsp.status)
+			{
+				$('#customer-id').val(rsp.customer_id);
+			}
+		});
+	});
+
+	$('body').on('change', '#customer-country', function (e) {
+		e.preventDefault();
+
+		let customer_id_path = "{{ route('getCustomerId')  }}";
+		$.get(customer_id_path, {source: 'country', id: e.target.value}).done((rsp) => {
+			if (rsp.status)
+			{
+				$('#customer-id').val(rsp.customer_id);
+			}
+		});
+	});
+  
 </script>
 
 
