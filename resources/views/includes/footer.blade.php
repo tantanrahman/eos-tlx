244df01de9fb00c263ce1c549b6e8b8396fea0a9
tantanrahman <social.tantanrahman@gmail.com>
2021-07-12 12:52:35 +0700

    Added New Style Master

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; <?php echo date("Y"); ?> <a href="https://tlx.co.id">TLX</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> Beta 1.1
+    </div>
+</footer>
