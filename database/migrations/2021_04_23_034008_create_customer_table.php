7e2e756e061567ee62d7dcab4c03de0312fd067f
tantanrahman <social.tantanrahman@gmail.com>
2021-07-13 16:35:12 +0700

    Added new Field in Customer

--- database/migrations/2021_04_23_034008_create_customer_table.php
+++ database/migrations/2021_04_23_034008_create_customer_table.php
@@ -15,17 +15,21 @@ class CreateCustomerTable extends Migration
     {
         Schema::create('customer', function (Blueprint $table) {
             $table->id();
-            $table->string('account_code',50)->unique();
-            $table->string('name',150);
+            $table->string('account_code',150)->unique();
+            $table->string('name');
             $table->string('company_name',150)->nullable();
             $table->text('address');
-            $table->foreignId('city_id')->references('id')->on('city')->onDelete('cascade');
-            $table->foreignId('country_id')->references('id')->on('country')->onDelete('cascade');
+            $table->bigInteger('city_id');
+            $table->string('city_name',150)->nullable();
+            $table->bigInteger('country_id');
+            $table->string('country_name',200)->nullable();
             $table->string('phone',60)->nullable();
             $table->enum('group', ['shipper','consignee'])->default('shipper');
             $table->string('postal_code',20)->nullable();
-            $table->string('sum_key',150)->nullable()->unique();
+            $table->string('apikey')->nullable()->unique();
             $table->string('api_password',150)->nullable();
+            $table->string('credit',200)->nullable();
+            $table->string('created_by');
             $table->timestamps();
         });
     }
